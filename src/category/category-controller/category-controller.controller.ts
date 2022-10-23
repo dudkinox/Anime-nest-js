170d501a25c14bd7979d3cf6f602d3a04d6f6e23
กีตาร์เอาเอง <guitar@odds.team>
2022-10-23 14:39:57 +0700

    :guitar: remove option bili

--- src/category/category-controller/category-controller.controller.ts
+++ src/category/category-controller/category-controller.controller.ts
@@ -1,5 +1,4 @@
 import { Get } from '@nestjs/common';
-import { Query } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
 import { CategoryService } from '../category-service/category-service.service';
 
@@ -8,19 +7,7 @@ export class CategoryController {
   constructor(private categoryService: CategoryService) {}
 
   @Get()
-  getCategory(
-    @Query('style') style: string,
-    @Query('page') page: string,
-    @Query('pagesize') pagesize: string,
-    @Query('platform') platform: string,
-    @Query('s_locale') s_locale: string,
-  ): any {
-    return this.categoryService.getCategory(
-      style,
-      page,
-      pagesize,
-      platform,
-      s_locale,
-    );
+  getCategory(): any {
+    return this.categoryService.getCategory();
   }
 }
