a8fc5518cadb788c9a8c4b8773bc392b1bf34589
กีตาร์เอาเอง <guitar@odds.team>
2022-10-23 14:01:36 +0700

    :guitar: update api billi

--- src/category/category-service/category-service.service.ts
+++ src/category/category-service/category-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { CategoryEnum } from 'src/enum/categoryEnum';
+// import { CategoryEnum } from 'src/enum/categoryEnum';
 
 @Injectable()
 export class CategoryService {
@@ -22,60 +22,16 @@ export class CategoryService {
     platform: string,
     s_locale: string,
   ): Promise<any> {
-    const result = { category: [] };
     const data = [];
+
     await this.httpService
       .get(
-        `https://api.bilibili.tv/intl/gateway/web/v2/ogv/index/items?page=${page}&pagesize=${pagesize}&platform=${platform}&s_locale=${s_locale}`,
+        `https://api.bilibili.tv/intl/gateway/web/v2/anime/more_anime?pn=${page}&ps=${pagesize}&platform=${platform}&s_locale=${s_locale}`,
       )
       .forEach((item) => {
-        switch (Number(style)) {
-          case CategoryEnum.Yaoi:
-            data.push(this.checkCategory(item, result.category, 'ยาโอย'));
-            break;
-          case CategoryEnum.Phantasy:
-            data.push(this.checkCategory(item, result.category, 'แฟนตาซี'));
-            break;
-          case CategoryEnum.Drama:
-            data.push(this.checkCategory(item, result.category, 'ดราม่า'));
-            break;
-          case CategoryEnum.Action:
-            data.push(this.checkCategory(item, result.category, 'ต่อสู้'));
-            break;
-          case CategoryEnum.Romantic:
-            data.push(this.checkCategory(item, result.category, 'โรแมนติก'));
-            break;
-          case CategoryEnum.SciFi:
-            data.push(this.checkCategory(item, result.category, 'ไซ-ไฟ'));
-            break;
-          case CategoryEnum.Avengers:
-            data.push(this.checkCategory(item, result.category, 'ผจญภัย'));
-            break;
-          case CategoryEnum.History:
-            data.push(
-              this.checkCategory(item, result.category, 'ประวัติศาสตร์'),
-            );
-            break;
-          case CategoryEnum.Comedy:
-            data.push(this.checkCategory(item, result.category, 'ตลก'));
-            break;
-          case CategoryEnum.life:
-            data.push(
-              this.checkCategory(item, result.category, 'ชีวิตประจำวัน'),
-            );
-            break;
-          case CategoryEnum.music:
-            data.push(this.checkCategory(item, result.category, 'ดนตรี'));
-            break;
-          case CategoryEnum.school:
-            data.push(this.checkCategory(item, result.category, 'โรงเรียน'));
-            break;
-          default:
-            data.push(this.checkCategory(item, result.category, ' '));
-            break;
-        }
+        data.push(item.data);
       });
 
-    return data[0];
+    return data;
   }
 }
